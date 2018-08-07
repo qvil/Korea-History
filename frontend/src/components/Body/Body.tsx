e8566a625e181ffbde1af83433406f3e8a7fff68
Taesu Hyeon <qvil@op.gg>
2018-08-07 14:45:37 +0900

    Apply Switch, Route

--- frontend/src/components/Body/Body.tsx
+++ frontend/src/components/Body/Body.tsx
@@ -1,6 +1,7 @@
 import gql from "graphql-tag";
 import * as React from "react";
 import { Query } from "react-apollo";
+import { Route, Switch } from "react-router-dom";
 import { Card } from "src/components";
 import BodyContainer from "./BodyContainer";
 
@@ -8,34 +9,44 @@ export interface IBodyProps {
   children?: any;
 }
 
+const DummyComponent = () => <div>Error :( 404 Not Found.</div>;
+
+const Joseon = () => (
+  <Query
+    query={gql`
+      {
+        kings {
+          title
+          image
+        }
+      }
+    `}
+  >
+    {({ loading, error, data }) => {
+      // tslint:disable-next-line:curly
+      if (loading) return <p>Loading...</p>;
+      // tslint:disable-next-line:curly
+      if (error) return <p>Error :(</p>;
+
+      return data.kings.map((value: any, index: number) => (
+        <Card key={index} image={value.image}>{`${index + 1}대 ${
+          value.title
+        }`}</Card>
+      ));
+    }}
+  </Query>
+);
+
 export default class Body extends React.Component<IBodyProps, any> {
   public render() {
     return (
-        <BodyContainer>
-          <Query
-            query={gql`
-              {
-                kings {
-                  title
-                  image
-                }
-              }
-            `}
-          >
-            {({ loading, error, data }) => {
-              // tslint:disable-next-line:curly
-              if (loading) return <p>Loading...</p>;
-              // tslint:disable-next-line:curly
-              if (error) return <p>Error :(</p>;
-
-              return data.kings.map((value: any, index: number) => (
-                <Card key={index} image={value.image}>{`${index + 1}대 ${
-                  value.title
-                }`}</Card>
-              ));
-            }}
-          </Query>
-        </BodyContainer>
+      <BodyContainer>
+        <Switch>
+          <Route exact={true} path="/" />
+          <Route path="/joseon" component={Joseon} />
+          <Route component={DummyComponent} />
+        </Switch>
+      </BodyContainer>
     );
   }
 }

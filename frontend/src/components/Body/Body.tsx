a044b2022a5e234bad9b8fb1d901a9ed6e5c378a
Taesu Hyeon <qvil@op.gg>
2018-08-07 20:18:08 +0900

    Apply Sub routing
    * Remove exact option at <Route  />

--- frontend/src/components/Body/Body.tsx
+++ frontend/src/components/Body/Body.tsx
@@ -1,6 +1,6 @@
 import * as React from "react";
 import { Route, Switch } from "react-router-dom";
-import { Joseon, NotFound } from "src/components/Body"
+import { Joseon, NotFound } from "src/components/Body";
 import BodyContainer from "./BodyContainer";
 
 export interface IBodyProps {
@@ -13,9 +13,7 @@ export default class Body extends React.Component<IBodyProps, any> {
       <BodyContainer>
         <Switch>
           <Route exact={true} path="/" />
-          <Route exact={true} path="/joseon" component={Joseon} />
-          // tslint:disable-next-line:jsx-no-lambda
-          {/* <Route render={() => <NotFound code={404} />} /> */}
+          <Route path="/joseon" component={Joseon} />
           <Route component={NotFound} />
         </Switch>
       </BodyContainer>
--- frontend/src/components/Body/Joseon.tsx
+++ frontend/src/components/Body/Joseon.tsx
@@ -2,7 +2,7 @@ import gql from "graphql-tag";
 import * as React from "react";
 import { Query } from "react-apollo";
 // import { withRouter } from "react-router";
-import { Route, withRouter } from "react-router-dom";
+import { Route, Switch, withRouter } from "react-router-dom";
 import { Card } from "src/components";
 
 export interface IJoseonProps {
@@ -13,45 +13,43 @@ export interface IJoseonProps {
 class Joseon extends React.Component<IJoseonProps, any> {
   public renderCustom = () => <div>Yes!</div>;
   public render() {
+    const { renderCustom, renderKingList } = this;
     const { path } = this.props.match;
 
-    console.log(this.props);
-    console.log(path);
     return (
       <React.Fragment>
-        <Route
-          // exact={true}
-          // path={`/1`}
-          path={"/joseon/1"}
-          // path={`${path}/1`}
-          // tslint:disable-next-line:jsx-no-lambda
-          component={this.renderCustom}
-        />
-        <Query
-          query={gql`
-            {
-              kings {
-                title
-                image
-              }
-            }
-          `}
-        >
-          {({ loading, error, data }) => {
-            // tslint:disable-next-line:curly
-            if (loading) return <p>Loading...</p>;
-            // tslint:disable-next-line:curly
-            if (error) return <p>Error :(</p>;
-
-            return data.kings.map((value: any, index: number) => (
-              <Card key={index} index={index} image={value.image}>{`${index +
-                1}대 ${value.title}`}</Card>
-            ));
-          }}
-        </Query>
+        <Switch>
+          <Route exact={true} path={path} component={renderKingList} />
+          <Route path={`${path}/1`} component={renderCustom} />
+        </Switch>
       </React.Fragment>
     );
   }
+
+  private renderKingList = () => (
+    <Query
+      query={gql`
+        {
+          kings {
+            title
+            image
+          }
+        }
+      `}
+    >
+      {({ loading, error, data }) => {
+        // tslint:disable-next-line:curly
+        if (loading) return <p>Loading...</p>;
+        // tslint:disable-next-line:curly
+        if (error) return <p>Error :(</p>;
+
+        return data.kings.map((value: any, index: number) => (
+          <Card key={index} index={index} image={value.image}>{`${index +
+            1}대 ${value.title}`}</Card>
+        ));
+      }}
+    </Query>
+  );
 }
 
 export default withRouter<any>(Joseon);
